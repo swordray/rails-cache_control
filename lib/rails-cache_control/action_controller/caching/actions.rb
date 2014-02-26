68a545fcb99a558d66612f45c4f021d49224ca40
swordray <swordray@gmail.com>
2014-02-26 17:36:50 +0800

    Update header options

--- lib/rails-cache_control/action_controller/caching/actions.rb
+++ lib/rails-cache_control/action_controller/caching/actions.rb
@@ -4,7 +4,7 @@ module ActionController
       class ActionCacheFilter
         def around_with_expires_in(controller, &block)
           expires_in = @store_options[:expires_in]
-          controller.expires_in(expires_in, @store_options) if expires_in
+          controller.expires_in(expires_in, public: true) if expires_in
           around_without_expires_in(controller, &block)
         end
         alias_method_chain :around, :expires_in
